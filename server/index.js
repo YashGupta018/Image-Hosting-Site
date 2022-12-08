28db126cca81e343d042ce00f8ca6f8ad03df827
YashGupta018 <ygupta2001@gmail.com>
2022-12-09 00:18:16 +0530

    At this point the server-working changes-made file

--- server/index.js
+++ server/index.js
@@ -2,17 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://yashgupta:mongodbAtlas@imagehosting.mp8h8sp.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const App = express();
 
-mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://shadow:vuzpdliwqKhZrVa8@cluster0.j7nnjb1.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server running on port : http://localhost:${PORT}`)})
 
 //listening on the specified port by the server
 
