51d3249925d7cb957595709572a2d57d907e4433
YashGupta018 <ygupta2001@gmail.com>
2022-12-08 12:19:22 +0530

    updates in node modules

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://yashgupta:mongodbAtlas@imagehosting.mp8h8sp.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
 
