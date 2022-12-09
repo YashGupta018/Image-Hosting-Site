aea0881e206a5bb313692936c7bb23d89374df2e
YashGupta018 <ygupta2001@gmail.com>
2022-12-09 15:29:50 +0530

    styles & basic front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from './images/memories.png';
+import imagehosting from './images/imagehosting.png';
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-
-        <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center"> Host Your Images </Typography>
-                <img src={memories} alt="memories" height="60"/>
+        <Container maxWidth="lg">
+            <AppBar className={ classes.appBar } position="static" color="inherit">
+                <Typography className={ classes.heading }variant="h2" align="center"> Post your Images </Typography>
+                <img className={ classes.image } src={imagehosting} alt="imagehosting" height="60"/>
             </AppBar>
-        </Container>
+            <Grow in>
 
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+        </Container>
     )
 }
 
